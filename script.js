c658dcacb3c229cd24922954b0cba3c135c92be4
Anand23456 <avulaanand59@gmail.com>
2023-10-19 16:06:37 +0530

    portfoliochange

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function sendNotification(e) {
+    var userEmail = e.values[1];
+    var userName = e.values[0]; 
+    var subject = "New Form Submission";
+    var message = "New submission from " + userName + " with email " + userEmail;
+    
+  
+    MailApp.sendEmail("avulaanand59@gmail.com", subject, message);
+
+}
+
